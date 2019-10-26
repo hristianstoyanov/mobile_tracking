a6c3da8654e3639c7e14151e5cb84a44cb2ecc2c
hristianstoyanov <hristian_st1@abv.bg>
2019-10-26 19:37:35 +0300

    Initial commit. Add structure packages.

---
+++ src/main/java/mobile/tracking/MobileTracking.java
@@ -0,0 +1,16 @@
+package mobile.tracking;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by Hristiyan on 26.10.2019 ã..
+ */
+@SpringBootApplication
+public class MobileTracking {
+
+    public static void main(String... args) {
+        SpringApplication.run(MobileTracking.class, args);
+    }
+}
